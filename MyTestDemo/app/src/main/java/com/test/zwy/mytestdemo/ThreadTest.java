da331f56d42b551193babe042400a0fc0284f5f9
zhuwenyu <zhuwenyu@fero.com.cn>
2016-06-16 11:01:22 +0800

    AsyncTask

--- MyTestDemo/app/src/main/java/com/test/zwy/mytestdemo/ThreadTest.java
+++ MyTestDemo/app/src/main/java/com/test/zwy/mytestdemo/ThreadTest.java
@@ -9,12 +9,20 @@ import android.os.Message;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.test.zwy.myapp.MyApplication;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by FERO010 on 2016/6/12.
@@ -22,7 +30,13 @@ import com.test.zwy.myapp.MyApplication;
 public class ThreadTest extends AppCompatActivity {
     TextView textThread;
     Button threadButton;
-    TextView asyncTaskButton;
+    private Button execute;
+    private Button cancel;
+    private ProgressBar progressBar;
+    private TextView textView;
+    MyTask mTask;
+
+    private static final String TAG = "ASYNC_TASK";
 
     private static final int MESSAGE_SCROLL_TO = 1;
     private static final int FRAME_COUNT = 30;
@@ -32,7 +46,6 @@ public class ThreadTest extends AppCompatActivity {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MESSAGE_SCROLL_TO:
-
                     textThread.setText("Change");
 
                     break;
@@ -47,14 +60,7 @@ public class ThreadTest extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.threadtest);
-        init();
-    }
-
-    private void init() {
-        textThread = (TextView) findViewById(R.id.textThread);
         threadButton = (Button) findViewById(R.id.threadButton);
-        asyncTaskButton = (Button) findViewById(R.id.asyncTaskButton);
-        //new DownLoadTask().execute();//启动任务
         threadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,56 +74,105 @@ public class ThreadTest extends AppCompatActivity {
                 }).start();
             }
         });
-    }
-}
 
-class DownLoadTask extends AsyncTask<Void, Integer, Boolean> {
-    Boolean result;
-    ProgressDialog progressDialog;
+        execute = (Button) findViewById(R.id.execute);
+        execute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //注意每次需new一个实例,新建的任务只能执行一次,否则会出现异常
+                mTask = new MyTask();
+                mTask.execute("http://www.baidu.com");
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();//显示进度对话框
-    }
-
-
-    @Override//处理耗时任务，在子线程中运行
-    protected Boolean doInBackground(Void... params) {
-        try {
-            while (true) {
-                int downloadPercent = doDownload();//虚构方法
-                publishProgress(downloadPercent);
-                if (downloadPercent >= 100) {
-                    break;
-                }
+                execute.setEnabled(false);
+                cancel.setEnabled(true);
             }
-        } catch (Exception e) {
-            return false;
+        });
+        cancel = (Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //取消一个正在执行的任务,onCancelled方法将会被调用
+                mTask.cancel(true);
+            }
+        });
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        textView = (TextView) findViewById(R.id.text_view);
+
+    }
+
+    private class MyTask extends AsyncTask<String, Integer, String> {
+        //onPreExecute方法用于在执行后台任务前做一些UI操作
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute() called");
+            textView.setText("loading...");
         }
-        return true;
-    }
 
-    @Override
-    protected void onProgressUpdate(Integer... values) {
-        //在这里更新下载进度（UI操作）
-        progressDialog.setMessage("Downloaded " + values[0] + "%");
-    }
+        //doInBackground方法内部执行后台任务,不可在此方法内修改UI
+        @Override
+        protected String doInBackground(String... params) {
+            Log.i(TAG, "doInBackground(Params... params) called");
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL("http://www.baidu.com");
+                connection = (HttpURLConnection)
+                        url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(8000);
+                connection.setReadTimeout(8000);
+                InputStream is = (InputStream) connection.getContent();
+                long total = connection.getContentLength();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buf = new byte[1024];
+                int count = 0;
+                int length = -1;
+                while ((length = is.read(buf)) != -1) {
+                    baos.write(buf, 0, length);
+                    count += length;
+                    //调用publishProgress公布进度,最后onProgressUpdate方法将被执行
+                    publishProgress((int) ((count / (float) total) * 100));
+                    //为了演示进度,休眠500毫秒
+                    Thread.sleep(500);
+                    return new String(baos.toByteArray(), "gb2312");
+                }
+            } catch (
+                    Exception e
+                    )
 
-    @Override
-    protected void onPostExecute(Boolean aBoolean) {
-        progressDialog.dismiss();//关闭进度对话框（任务收尾工作）
-        //在这里提示下载结果
-        if (result) {
-            Toast.makeText(MyApplication.getContext(), "Download succeeded",
-                    Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(MyApplication.getContext(), "Download failed",
-                    Toast.LENGTH_SHORT).show();
+            {
+                Log.e(TAG, e.getMessage());
+            }
+
+            return null;
         }
-    }
 
-    private int doDownload() {
+        //onProgressUpdate方法用于更新进度信息
+        @Override
+        protected void onProgressUpdate(Integer... progresses) {
+            Log.i(TAG, "onProgressUpdate(Progress... progresses) called");
+            progressBar.setProgress(progresses[0]);
+            textView.setText("loading..." + progresses[0] + "%");
+        }
 
-        return 0;
+        //onPostExecute方法用于在执行完后台任务后更新UI,显示结果
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(TAG, "onPostExecute(Result result) called");
+            textView.setText(result);
+
+            execute.setEnabled(true);
+            cancel.setEnabled(false);
+        }
+
+        //onCancelled方法用于在取消执行中的任务时更改UI
+        @Override
+        protected void onCancelled() {
+            Log.i(TAG, "onCancelled() called");
+            textView.setText("cancelled");
+            progressBar.setProgress(0);
+
+            execute.setEnabled(true);
+            cancel.setEnabled(false);
+        }
     }
 }
